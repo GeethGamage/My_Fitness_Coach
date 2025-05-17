937d29c908e0ed255159d5150d398a58a8ad8492
unknown <geethmadushankagamage@gmail.com>
2025-05-17 13:42:18 +0800

    first commit

---
+++ src/main/java/com/My_Fitness_Coach/MyFitnessCoachApplication.java
@@ -0,0 +1,13 @@
+package com.My_Fitness_Coach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitnessCoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFitnessCoachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/My_Fitness_Coach/MyFitnessCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.My_Fitness_Coach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitnessCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
